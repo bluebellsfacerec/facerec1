b6024c0fbfc797a68003892a4f568d66e2a753ec
bluebellsfacerec <67471102+bluebellsfacerec@users.noreply.github.com>
2020-06-28 17:08:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var uploadUrl = "https://face-recognition-masked-unmasked.cognitiveservices.azure.com/customvision/v3.0/Prediction/68e29f1e-edea-4108-85e4-4a92480efe57/classify/iterations/Mask%20Immune%20Face%20Recognition%20System/image";
+
+
+var linkUrl = "";
+
+
+function upload(file) {
+    let xhr = new XMLHttpRequest();
+
+    // track upload progress
+    xhr.upload.onprogress = function(event) {
+        console.log(`Uploaded ${event.loaded} of ${event.total}`);
+    };
+
+    // track completion: both successful or not
+    xhr.onloadend = function() {
+        if (xhr.status == 200) {
+            console.log("success");
+        } else {
+            console.log("error " + this.status);
+        }
+    };
+
+    xhr.open("POST", uploadUrl);
+    xhr.setRequestHeader('Prediction-Key', '04b3d203a0b74db08bba5834f5f43223');
+    xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+    xhr.send(file);
+    xhr.onload = () => {
+        //document.getElementById('data').innerHTML = xhr.response
+        alert(xhr.response);
+    };
+}
