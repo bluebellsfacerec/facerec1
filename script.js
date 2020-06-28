261bd19236f822539ef9cf61421ce0876abe0e34
bluebellsfacerec <67471102+bluebellsfacerec@users.noreply.github.com>
2020-06-28 19:46:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,12 @@ function upload(file) {
         alert(xhr.response);
     };
 }
+
+function PreviewImage() {
+    var oFReader = new FileReader();
+    oFReader.readAsDataURL(document.getElementById("upload-image").files[0]);
+
+    oFReader.onload = function(oFREvent) {
+        document.getElementById("uploadPreview").src = oFREvent.target.result;
+    };
+};
